a934cb7e582169639d5edd045fb17ef031fe7f08
nnouran <96547311+nouranMo@users.noreply.github.com>
2024-05-07 00:33:07 +0400

    routing authorization

--- volunteens/src/main/java/com/example/aswe/volunteens/controller/TempController.java
+++ volunteens/src/main/java/com/example/aswe/volunteens/controller/TempController.java
@@ -2,7 +2,12 @@ package com.example.aswe.volunteens.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class TempController {
@@ -12,14 +17,26 @@ public class TempController {
     }
 
     @GetMapping("opportunities")
-    public ModelAndView opportunities() {
-        return new ModelAndView("opportunities.html");
+    public ModelAndView opportunities(HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+           
+            return new ModelAndView("redirect:/accessDenied");
+        }
+        return new ModelAndView("volunteer.html");
+    
     }
+    
 
     @GetMapping("donate")
-    public ModelAndView donate() {
+    public ModelAndView donate(HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+           
+            return new ModelAndView("redirect:/accessDenied");
+        }
         return new ModelAndView("donate.html");
+    
     }
+    
 
     @GetMapping("service")
     public ModelAndView service() {
@@ -37,12 +54,30 @@ public class TempController {
     }
 
     @GetMapping("volunteer")
-    public ModelAndView volunteer() {
+    public ModelAndView volunteer(HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+           
+            return new ModelAndView("redirect:/accessDenied");
+        }
         return new ModelAndView("volunteer.html");
+    
     }
+        
+    
 
     @GetMapping("postOpportunity")
-    public ModelAndView postOpportunity() {
+    public ModelAndView postOpportunity(HttpSession session) {
+        if (session.getAttribute("orgId") == null) {
+           
+            return new ModelAndView("redirect:/accessDenied");
+        }
         return new ModelAndView("postOpportunity.html");
+    
     }
+
+    @GetMapping("accessDenied")
+    public ModelAndView accessDenied() {
+        return new ModelAndView("404.html");
+    }
+    
 }
