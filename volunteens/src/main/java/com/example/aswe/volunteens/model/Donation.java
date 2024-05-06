fb9269db3fc0ec8f29a5a03bee087fed8fb37851
gitzuzu <Zeina2101569@miuegypt.edu.eg>
2024-05-06 20:46:24 +0300

    modals

---
+++ volunteens/src/main/java/com/example/aswe/volunteens/model/Donation.java
@@ -0,0 +1,118 @@
+package com.example.aswe.volunteens.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Objects;
+
+@Entity
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long donationId;
+
+    @Column(name="amountDonated")
+    private String amountDonated;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "donationDate")
+    private LocalDate donationDate;
+
+    public Donation() {
+    }
+
+    public Donation(Long donationId, String amountDonated, User user, LocalDate donationDate) {
+        this.donationId = donationId;
+        this.amountDonated = amountDonated;
+        this.user = user;
+        this.donationDate = donationDate;
+    }
+
+    public Long getDonationId() {
+        return this.donationId;
+    }
+
+    public void setDonationId(Long donationId) {
+        this.donationId = donationId;
+    }
+
+    public String getAmountDonated() {
+        return this.amountDonated;
+    }
+
+    public void setAmountDonated(String amountDonated) {
+        this.amountDonated = amountDonated;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getDonationDate() {
+        return this.donationDate;
+    }
+
+    public void setDonationDate(LocalDate donationDate) {
+        this.donationDate = donationDate;
+    }
+
+    public Donation donationId(Long donationId) {
+        setDonationId(donationId);
+        return this;
+    }
+
+    public Donation amountDonated(String amountDonated) {
+        setAmountDonated(amountDonated);
+        return this;
+    }
+
+    public Donation user(User user) {
+        setUser(user);
+        return this;
+    }
+
+    public Donation donationDate(LocalDate donationDate) {
+        setDonationDate(donationDate);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Donation)) {
+            return false;
+        }
+        Donation donation = (Donation) o;
+        return Objects.equals(donationId, donation.donationId) && Objects.equals(amountDonated, donation.amountDonated) && Objects.equals(user, donation.user) && Objects.equals(donationDate, donation.donationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(donationId, amountDonated, user, donationDate);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " donationId='" + getDonationId() + "'" +
+            ", amountDonated='" + getAmountDonated() + "'" +
+            ", user='" + getUser() + "'" +
+            ", donationDate='" + getDonationDate() + "'" +
+            "}";
+    }
+}
---
+++ volunteens/src/main/java/com/example/aswe/volunteens/model/Opportunity.java
@@ -0,0 +1,153 @@
+package com.example.aswe.volunteens.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Objects;
+
+@Entity
+public class Opportunity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name = "id")
+    private Long opportunityId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "location", nullable = false) // 'online' or 'location
+    private String location;
+
+    @Column(name = "requirements", nullable = false)
+    private String requirements;
+
+    @ManyToOne
+    @JoinColumn(name = "organization_id", nullable = false)
+    private Organization organization;
+
+    public Opportunity() {
+    }
+
+    public Opportunity(Long opportunityId, String title, String description, String location, String requirements, Organization organization) {
+        this.opportunityId = opportunityId;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.requirements = requirements;
+        this.organization = organization;
+    }
+
+    public Long getOpportunityId() {
+        return this.opportunityId;
+    }
+
+    public void setOpportunityId(Long opportunityId) {
+        this.opportunityId = opportunityId;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getRequirements() {
+        return this.requirements;
+    }
+
+    public void setRequirements(String requirements) {
+        this.requirements = requirements;
+    }
+
+    public Organization getOrganization() {
+        return this.organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+
+    public Opportunity opportunityId(Long opportunityId) {
+        setOpportunityId(opportunityId);
+        return this;
+    }
+
+    public Opportunity title(String title) {
+        setTitle(title);
+        return this;
+    }
+
+    public Opportunity description(String description) {
+        setDescription(description);
+        return this;
+    }
+
+    public Opportunity location(String location) {
+        setLocation(location);
+        return this;
+    }
+
+    public Opportunity requirements(String requirements) {
+        setRequirements(requirements);
+        return this;
+    }
+
+    public Opportunity organization(Organization organization) {
+        setOrganization(organization);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Opportunity)) {
+            return false;
+        }
+        Opportunity opportunity = (Opportunity) o;
+        return Objects.equals(opportunityId, opportunity.opportunityId) && Objects.equals(title, opportunity.title) && Objects.equals(description, opportunity.description) && Objects.equals(location, opportunity.location) && Objects.equals(requirements, opportunity.requirements) && Objects.equals(organization, opportunity.organization);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(opportunityId, title, description, location, requirements, organization);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " opportunityId='" + getOpportunityId() + "'" +
+            ", title='" + getTitle() + "'" +
+            ", description='" + getDescription() + "'" +
+            ", location='" + getLocation() + "'" +
+            ", requirements='" + getRequirements() + "'" +
+            ", organization='" + getOrganization() + "'" +
+            "}";
+    }
+    
+}
