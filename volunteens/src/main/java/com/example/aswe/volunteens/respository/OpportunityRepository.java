6fec540c466ed1fe4430da0d7a69b88156fc066c
nnouran <96547311+nouranMo@users.noreply.github.com>
2024-05-15 23:36:57 +0400

    pagination

--- volunteens/src/main/java/com/example/aswe/volunteens/respository/OpportunityRepository.java
+++ volunteens/src/main/java/com/example/aswe/volunteens/respository/OpportunityRepository.java
@@ -1,11 +1,15 @@
 package com.example.aswe.volunteens.respository;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.aswe.volunteens.model.Opportunity;
 
-
+import java.util.List;
 
 public interface OpportunityRepository extends JpaRepository<Opportunity,Long>{
     boolean existsByTitle(String title);
+    Page<Opportunity> findAll(Pageable pageable);
 }
