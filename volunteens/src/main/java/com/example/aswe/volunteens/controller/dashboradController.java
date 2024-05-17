b025faafadf830f6a103325926ad085ca7157c6d
EslamAhmedv <125676587+EslamAhmedv@users.noreply.github.com>
2024-05-17 18:40:47 +0300

    Users Management View

--- volunteens/src/main/java/com/example/aswe/volunteens/controller/dashboradController.java
+++ volunteens/src/main/java/com/example/aswe/volunteens/controller/dashboradController.java
@@ -1,17 +1,24 @@
 package com.example.aswe.volunteens.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.aswe.volunteens.model.User;
+import com.example.aswe.volunteens.respository.UserRepositry;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class dashboradController {
+
+     @Autowired
+    private UserRepositry UserRepositry;
 
     @GetMapping("dashboard")
     public ModelAndView dashboard(HttpSession session, Model model){
@@ -22,6 +29,19 @@ public class dashboradController {
         }
         return new ModelAndView("redirect:/accessDenied");
 
+    }
+
+
+    @GetMapping("/users")
+    public ModelAndView showUsers(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+        if (user != null && "admin@gmail.com".equals(user.getEmail())) {
+            List<User> users = UserRepositry.findAll();
+            ModelAndView modelAndView = new ModelAndView("Userdash");
+            modelAndView.addObject("users", users);
+            return modelAndView;
+        }
+        return new ModelAndView("redirect:/accessDenied");
     }
     
 }
