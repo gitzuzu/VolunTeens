7778300f8ad0b0c694ffc04a5f09720c39d2354b
gitzuzu <Zeina2101569@miuegypt.edu.eg>
2024-05-20 18:43:46 +0300

    feedback update

--- volunteens/src/main/java/com/example/aswe/volunteens/controller/TestimonialController.java
+++ volunteens/src/main/java/com/example/aswe/volunteens/controller/TestimonialController.java
@@ -1,6 +1,8 @@
 package com.example.aswe.volunteens.controller;
 
+import com.example.aswe.volunteens.model.Organization;
 import com.example.aswe.volunteens.model.Testimonial;
+import com.example.aswe.volunteens.model.User;
 import com.example.aswe.volunteens.service.TestimonialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 @Controller
@@ -20,26 +23,47 @@ public class TestimonialController {
     private TestimonialService testimonialService;
 
     @GetMapping("/testimonial")
-    public ModelAndView showTestimonials() {
-        ModelAndView modelAndView = new ModelAndView("testimonial");
-        modelAndView.addObject("testimonials", testimonialService.getAllTestimonials());
-        return modelAndView;
+    public ModelAndView showTestimonials(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+        Organization organization = (Organization) session.getAttribute("org");
+        
+        if (user == null && organization == null) {
+
+            return new ModelAndView("redirect:/accessDenied");
+        }
+        else{
+            ModelAndView modelAndView = new ModelAndView("testimonial");
+            modelAndView.addObject("testimonials", testimonialService.getAllTestimonials());
+            return modelAndView;
+        }
+       
     }
 
     @GetMapping("/testimonial/new")
-    public ModelAndView showTestimonialForm() {
-        ModelAndView modelAndView = new ModelAndView("Testimonialform");
-        modelAndView.addObject("testimonial", new Testimonial());
-        return modelAndView;
+    public ModelAndView showTestimonialForm(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+        Organization organization = (Organization) session.getAttribute("org");
+        
+        if (user == null && organization == null) {
+
+            return new ModelAndView("redirect:/accessDenied");
+        }
+        else{
+            ModelAndView modelAndView = new ModelAndView("Testimonialform");
+            modelAndView.addObject("testimonial", new Testimonial());
+            return modelAndView;
+        
+        }
+
     }
 
     @PostMapping("/testimonial")
-    public ModelAndView submitTestimonial(@Valid @ModelAttribute Testimonial testimonial, BindingResult result) {
+    public ModelAndView submitTestimonial(@Valid @ModelAttribute Testimonial testimonial, BindingResult result , HttpSession session) {
         if (result.hasErrors()) {
             ModelAndView modelAndView = new ModelAndView("Testimonialform");
             return modelAndView;
         }
-        testimonialService.saveTestimonial(testimonial);
+        testimonialService.saveTestimonial(testimonial , session);
         return new ModelAndView("redirect:/testimonial");
     }
 
